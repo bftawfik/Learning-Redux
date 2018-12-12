4bd126c6236c1328b806c663a8da314ddef1bc4b
Bishoy Farouk Tawfik <bftawfik@gmail.com>
2018-12-12 13:51:13 +0200

    end of chapter 3

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -1,28 +1,25 @@
-import C from './constants'
-import storeFactory from './store'
+import C from './constants';
+import storeFactory from './store';
 
-const initialState = (localStorage['redux-store']) ?
-	JSON.parse(localStorage['redux-store']) :
-	{}
-
+const initialState = JSON.parse(localStorage['redux-store'] || "{}") ;
+console.log(initialState);
 const saveState = () => {
-	const state = JSON.stringify(store.getState())
-  localStorage['redux-store'] = state
+	const state = JSON.stringify(store.getState());
+  localStorage['redux-store'] = state;
 }
 
-const store = storeFactory(initialState)
-
-store.subscribe(saveState)
+const store = storeFactory();
+store.subscribe(saveState);
 
 store.dispatch({
 	type: C.ADD_DAY,
 	payload: {
-		"resort": "Mt Shasta",
-		"date": "2016-10-28",
-		"powder": true,
-		"backcountry": true
-	}
-})
+    "resort": "Mt Shasta",
+    "date": "2016-10-28",
+    "powder": false,
+    "backcountry": true
+  }
+});
 
 store.dispatch({
 	type: C.ADD_DAY,
@@ -32,7 +29,7 @@ store.dispatch({
 		"powder": true,
 		"backcountry": false
 	}
-})
+});
 
 store.dispatch({
 	type: C.ADD_DAY,
@@ -42,4 +39,4 @@ store.dispatch({
 		"powder": false,
 		"backcountry": true
 	}
-})
+});
--- my-project/src/store/index.js
+++ my-project/src/store/index.js
@@ -1,33 +1,28 @@
-import C from '../constants'
-import appReducer from './reducers'
-import { createStore, applyMiddleware } from 'redux'
+import C from '../constants';
+import appReducer from './reducers';
+import { createStore, applyMiddleware } from 'redux';
 
 const consoleMessages = store => next => action => {
 
-	let result
+  let result;
 
-	console.groupCollapsed(`dispatching action => ${action.type}`)
-	console.log('ski days', store.getState().allSkiDays.length)
-	result = next(action)
+  console.groupCollapsed(`dispatching action => ${action.type}`);
+  console.log('ski days', store.getState().allSkiDays.length);
 
-	let { allSkiDays, goal, errors, resortNames } = store.getState()
+  result = next(action);
+  let { allSkiDays, goal, errors, resortNames } = store.getState();
 
-	console.log(`
-
-		ski days: ${allSkiDays.length}
-		goal: ${goal}
-		fetching: ${resortNames.fetching}
-		suggestions: ${resortNames.suggestions}
-		errors: ${errors.length}
-
-	`)
-
-	console.groupEnd()
-
-	return result
-
+  console.log(`
+    ski days: ${allSkiDays.length}
+    goal: ${goal}
+    fetching: ${resortNames.fetching}
+    sggestions: ${resortNames.sggestions}
+    errors: ${errors.length}
+  `);
+  console.groupEnd();
+  return result;
 }
 
 export default (initialState={}) => {
-	return applyMiddleware(consoleMessages)(createStore)(appReducer, initialState)
+  return applyMiddleware(consoleMessages)(createStore)(appReducer, initialState);
 }
