a4756221ba50e5b518747f95ca791ae15aa0d6a1
Bishoy Farouk Tawfik <bftawfik@gmail.com>
2018-12-27 16:54:43 +0200

    end of lesson 2 chapter 5

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -4,16 +4,25 @@ import { render } from 'react-dom'
 import routes from './routes'
 import sampleData from './initialState.json'
 
+import {Provider} from 'react-redux';
+
+import storeFactory from './store';
+
 const initialState = (localStorage["redux-store"]) ?
     JSON.parse(localStorage["redux-store"]) :
     sampleData
 
+console.log(initialState);
 const saveState = () =>
     localStorage["redux-store"] = JSON.stringify(store.getState())
 
+const store = storeFactory(initialState);
+store.subscribe(saveState);
+
 window.React = React
+window.store = store;
 
 render(
-	routes,
+  <Provider store={store}>{routes}</Provider>,
   document.getElementById('react-container')
 )
